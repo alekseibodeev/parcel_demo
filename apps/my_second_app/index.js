c198a1c453eba961026194ca44d750a17524c149
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-08-26 20:24:09 +0500

    Initial commit

---
+++ apps/my_second_app/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const countDisplay = document.querySelector("#count");
+const countUpButton = document.querySelector("#count-up");
+
+let count = 0;
+
+const renderCount = (count) => {
+  countDisplay.textContent = count;
+};
+
+const incrementCount = () => ++count;
+
+countUpButton.addEventListener("click", () => {
+  renderCount(incrementCount());
+});
